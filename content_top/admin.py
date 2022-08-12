5feed440ab2d49999d9e395f1aaa5e220c2995d8
Zilvaro <zilvinas@digitalcase.ch>
2022-08-12 14:28:59 +0000

    content_top model added

--- content_top/admin.py
+++ content_top/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Content
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Content)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
