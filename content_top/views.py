1bc342bfe657ac3347ac9dd2f3a92c5897043b37
Zilvaro <zilvinas@digitalcase.ch>
2022-08-15 12:25:14 +0000

    content-detail connected

--- content_top/views.py
+++ content_top/views.py
@@ -7,5 +7,20 @@ def content(request):
    return render(request, 'content.html')
 
 class ContentList(generic.ListView):
-    model = Content
-    template_name = 'content.html'
+   model = Content
+   template_name = 'content.html'
+
+
+class ContentDetail(View):
+   
+   def get(self, request, slug, *args, **kwargs):
+        queryset = Content.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+
+        return render(
+            request,
+            "content_detail.html",
+            {
+                "post": post
+            },
+        )
