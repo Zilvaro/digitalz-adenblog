5b47a41fbcbc2950faa2dc83b87e43f88edd7c9d
Zilvaro <zilvinas@digitalcase.ch>
2022-09-13 08:08:58 +0000

    user can chose different image height displayed on larger screens than on small

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ from django.urls import reverse
 STATUS = ((0, "Draft"), (1, "Published"))
 IMAGECOUNTMOBILE = ((12, "Whole Page"), (6, "2xPage"))
 IMAGECOUNTDESKTOP = ((12, "Whole Page"), (8, "2/3 of Page"), (6, "Half of Page"), (4, "1/3 of Page"), (3, "1/4 of Page"))
-IMAGEHEIGHT = ((32, "320px"), (27, "270px"), (16, "160px"), (8, "88px"), (5, "50px") )
+IMAGEHEIGHT = ((40, "400px"), (32, "320px"), (27, "270px"), (20, "220px"), (16, "160px"), (8, "88px"), (5, "50px") )
 IMAGEPLACE = ((1, "Image-as-background"), (2, "Image-on-side"), (3, "Image-on-top"))
 TEXTBACKGROUND = ((1, "No background"), (2, "Put background"))
 
@@ -77,7 +77,7 @@ class HeroContent(models.Model):
     images_on_mobile_page = models.IntegerField(choices=IMAGECOUNTMOBILE, default=12)
     images_on_desktop = models.IntegerField(choices=IMAGECOUNTDESKTOP, default=6)
     image_height = models.IntegerField(choices=IMAGEHEIGHT, default=27)
-    image_place = models.IntegerField(choices=IMAGEPLACE, default=2)
+    image_place = models.IntegerField(choices=IMAGEPLACE, default=1)
     image_order = models.IntegerField(default=1)
     updated_on = models.DateTimeField(auto_now=True)
     hero_content = models.TextField(blank=True)
